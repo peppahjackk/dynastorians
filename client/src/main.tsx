d9b747dd510c6efb41f04feda0eccb9a2f895430
Ted Moke <iam@tedmoke.com>
2023-11-06 21:53:54 -0500

    Setup router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,13 +7,15 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {  RouterProvider } from 'react-router-dom';
+import { router } from "./router.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
