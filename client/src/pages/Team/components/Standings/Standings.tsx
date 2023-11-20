451e639b5455fcd3f7d292f0adc4869722a40611
Ted Moke <iam@tedmoke.com>
2023-11-19 20:51:07 -0500

    Fix format settins

--- client/src/pages/Team/components/Standings/Standings.tsx
+++ client/src/pages/Team/components/Standings/Standings.tsx
@@ -1,41 +1,77 @@
-import React from 'react'
-import { useTeamsQuery } from '../../queries/useTeamsQuery'
-import { Box, Stack, Typography } from '@mui/material';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import React from "react";
+import { useTeamsQuery } from "../../queries/useTeamsQuery";
+import { Stack, Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
   {
-    field: 'num', headerName: "#", width: 35, valueGetter: (params) => params.api.getRowIndexRelativeToVisibleRows(params.id) + 1
+    field: "num",
+    headerName: "#",
+    width: 35,
+    valueGetter: (params) =>
+      params.api.getRowIndexRelativeToVisibleRows(params.id) + 1,
   },
-  { field: 'id', headerName: "Team", width: 200, valueGetter: ({ row }) => (row.rosters[0].team_name) },
-  { field: 'bestRecord', headerName: "Best Record", width: 150, valueGetter: ({ row }) => (`${row.stats.bestRecord.wins}-${row.stats.bestRecord.losses}  (${row.stats.bestRecord.season})`) },
   {
-    field: 'winPct', headerName: "Win %", width: 100, valueGetter: ({ row }) =>
-      (row.stats.winPct * 100).toFixed(2) + '%'
+    field: "id",
+    headerName: "Team",
+    width: 200,
+    valueGetter: ({ row }) => row.rosters[0].team_name,
   },
-  { field: 'record', headerName: 'Record', width: 100, valueGetter: ({ row }) => `${row.stats.wins}-${row.stats.losses}` },
-  { field: 'averagePoints', headerName: 'Average Points', width: 100, valueGetter: ({ row }) => row?.stats?.averagePoints?.toFixed(2) }
-]
+  {
+    field: "bestRecord",
+    headerName: "Best Record",
+    width: 150,
+    valueGetter: ({ row }) =>
+      `${row.stats.bestRecord.wins}-${row.stats.bestRecord.losses}  (${row.stats.bestRecord.season})`,
+  },
+  {
+    field: "winPct",
+    headerName: "Win %",
+    width: 100,
+    valueGetter: ({ row }) => (row.stats.winPct * 100).toFixed(2) + "%",
+  },
+  {
+    field: "record",
+    headerName: "Record",
+    width: 100,
+    valueGetter: ({ row }) => `${row.stats.wins}-${row.stats.losses}`,
+  },
+  {
+    field: "averagePoints",
+    headerName: "Average Points",
+    width: 100,
+    valueGetter: ({ row }) => row?.stats?.averagePoints?.toFixed(2),
+  },
+];
 
-export const Standings = ({ league_id }: { league_id: string, season?: string }) => {
-  const { data: teamsData, isLoading: teamsIsLoading, error: teamsError } = useTeamsQuery({ league_id });
+export const Standings = ({
+  league_id,
+}: {
+  league_id: string;
+  season?: string;
+}) => {
+  const {
+    data: teamsData,
+    isLoading: teamsIsLoading,
+    error: teamsError,
+  } = useTeamsQuery({ league_id });
 
   let content;
   if (teamsIsLoading) {
-    content = <div>loading teams...</div>
+    content = <div>loading teams...</div>;
   }
 
   if (teamsError) {
-    content = <div>Teams error: {teamsError.message}</div>
+    content = <div>Teams error: {teamsError.message}</div>;
   }
 
   if (teamsData == null || teamsData.length <= 0) {
-    content = <div>No teams data found!</div>
+    content = <div>No teams data found!</div>;
   }
 
   if (teamsData != null) {
-    const rows = teamsData.map(team => ({ ...team, id: team._id }))
-    content = <DataGrid columns={columns} rows={rows} />
+    const rows = teamsData.map((team) => ({ ...team, id: team._id }));
+    content = <DataGrid columns={columns} rows={rows} />;
   }
 
   return (
@@ -43,5 +79,5 @@ export const Standings = ({ league_id }: { league_id: string, season?: string })
       <Typography variant="h4">League Stats - All Time</Typography>
       {content}
     </Stack>
-  )
-}
+  );
+};
