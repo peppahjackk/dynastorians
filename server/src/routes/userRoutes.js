3e5c8a22fc86873ceecb523ea6a4162c56ad31bf
Ted Moke <iam@tedmoke.com>
2023-11-21 22:52:26 -0500

    Add signup route

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -7,6 +7,7 @@ router.get("/current", userController.getCurrentUser)
 router.get("/:id", userController.getUserById);
 router.post("/", userController.createUser);
 router.post("/signin", userController.signIn);
+router.post("/signup", userController.signUp);
 router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
