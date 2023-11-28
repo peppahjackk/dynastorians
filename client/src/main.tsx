69e744fc10ff2972923856ef4a18961eae5a9876
Ted Moke <iam@tedmoke.com>
2023-11-27 22:38:18 -0500

    Continue adding auth to frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,8 +7,22 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
+import { initializeApp } from "firebase/app";
 
 const queryClient = new QueryClient();
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: "dynastorians.firebaseapp.com",
+  projectId: "dynastorians",
+  storageBucket: "dynastorians.appspot.com",
+  messagingSenderId: "614940800097",
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
