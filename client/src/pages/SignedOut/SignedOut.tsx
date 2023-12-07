4f0a8d8a88fd5ee59ad944fc26b0a7a17cae45ed
Ted Moke <iam@tedmoke.com>
2023-12-07 13:42:20 -0500

    Add login/signup screen ui

--- client/src/pages/SignedOut/SignedOut.tsx
+++ client/src/pages/SignedOut/SignedOut.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Button } from "@mui/material";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useAuth } from "../../features/auth/useAuth";
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 export const SignedOut = () => {
   const auth = useAuth();
@@ -9,7 +11,7 @@ export const SignedOut = () => {
 
   const handleSignup = () => {
     setIsProcessing(true);
-    
+
     createUserWithEmailAndPassword(
       auth,
       "theodore.moke@gmail.com",
@@ -25,9 +27,44 @@ export const SignedOut = () => {
         setIsProcessing(false);
       });
   };
+
+  const handleLogin = () => {};
+
   return (
-    <div>
-      <Button disabled={isProcessing} onClick={handleSignup}>Sign Up</Button>
-    </div>
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="space-around"
+      spacing={6}
+      padding={2}
+    >
+      <Grid item xs={12}>
+        <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
+          <Typography variant="h2">Dynastorians</Typography>
+          <Typography variant="body1" textAlign="center">
+            Ever wondered what the highest score in your dynasty league was? Or
+            who your biggest rival is? Sign up to find out!
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Box display="flex" justifyContent="center">
+          <Button disabled={isProcessing} onClick={handleLogin}>
+            Login
+          </Button>
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Box display="flex" justifyContent="center">
+          <Button
+            variant="contained"
+            disabled={isProcessing}
+            onClick={handleSignup}
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
