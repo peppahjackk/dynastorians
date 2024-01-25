accf022907d37256f92f388c2abf4435c0952299
Ted Moke <iam@tedmoke.com>
2024-01-24 20:05:22 -0500

    Allow fetching all leagues from hook

--- client/src/pages/Leagues/queries/useLeaguesQuery.ts
+++ client/src/pages/Leagues/queries/useLeaguesQuery.ts
@@ -2,14 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { League } from "../types/league";
 
-function fetchLeagues({ userId }: { userId?: string }): Promise<League[]> {
-  return axios
-    .get(`/api/users/leagues/${userId}`)
-    .then((response) => response.data);
+function fetchLeagues({ userId }: { userId?: number }): Promise<League[]> {
+  const url = userId ? `/api/users/leagues/${userId}` : `/api/leagues`;
+  return axios.get(url).then((response) => response.data);
 }
 
 // ✅ data will be `League[] | undefined` here
-export function useLeaguesQuery({ userId }: { userId?: string }) {
+export function useLeaguesQuery({ userId }: { userId?: number }) {
   return useQuery({
     queryKey: ["leagues", userId],
     queryFn: () => fetchLeagues({ userId }),
